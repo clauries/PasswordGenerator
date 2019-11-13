9f235114c445361afb3ebb82c003dc48ed216f99
clauries <55369403+clauries@users.noreply.github.com>
2019-11-13 01:26:53 -0800

    Updated code to write password in the box

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,47 +9,51 @@ const upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M
 const symbolsArr = ["!", "@", "#", "$", "%", "&", "*", "?"]
 const numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-generateBtn.addEventListener("click", function () {
-    //Questions for user to customize password
-    let lengthWant = prompt("How many characters do you want (min 8, max 128)?");
-    let lowerWant = confirm("Do you want lowercase characters?");
-    let upperWant = confirm("Do you want uppercase characters?");
-    let symbolsWant = confirm("Do you want special characters included?");
-    let numbersWant = confirm("Do you want numbers included?");
-    
+generateBtn.addEventListener("click", function (event) {
+   
     //Final password variables
     let optionsWanted = "";
     let builtPasswordArr = [];
-    let builtPassword = builtPasswordArr.join('');
-    passwordBox.innerHTML = builtPassword;
 
-    
-    //If true, join array into the optionsWanted string (an array of characters)
-
-    if (lengthWant <= 7 || lengthWant >= 129) {
-        alert("Please enter a number between 8 and 128");
-    } else {
   
-    if (lowerWant) {
-        optionsWanted += lowerArr.join("");
-    }
-    if (upperWant) {
-        optionsWanted += upperArr.join("");
-    }
-    if (symbolsWant) {
-        optionsWanted += symbolsArr.join("");
-    }
-    if (numbersWant) {
-        optionsWanted += numbersArr.join("");
-    }}
+    //Questions for user to customize password
+    let lengthWant = prompt("How many characters do you want (min 8, max 128)?");
 
-    //Loops thru the optionsWanted array and randomly pulls characters until lengthWant is fullfilled
-    for (var i = 0; i < lengthWant; i++) {
-        let randomOptionIndex = Math.floor(Math.random() * optionsWanted.length);
-        let randomOption = optionsWanted[randomOptionIndex - 1];
-        builtPasswordArr.push(randomOption);
-        //passwordBox.textContent("builtPasswordArr");
+    if (lengthWant >= 8 && lengthWant <= 128) {
+        let symbolsWant = confirm("Do you want special characters included?");
+        let numbersWant = confirm("Do you want numbers included?");
+        let lowerWant = confirm("Do you want lowercase characters?");
+        let upperWant = confirm("Do you want uppercase characters?");
+
+        //If true, join array into the optionsWanted string (an array of characters)
+        if (symbolsWant) {
+            optionsWanted += symbolsArr.join("");
         }
+        if (numbersWant) {
+            optionsWanted += numbersArr.join("");
+        }
+        if (lowerWant) {
+            optionsWanted += lowerArr.join("");
+        }
+        if (upperWant) {
+            optionsWanted += upperArr.join("");
+        }
+
+        //Loops thru the optionsWanted array and randomly pulls characters until lengthWant is fullfilled
+        for (var i = 0; i < lengthWant; i++) {
+            let randomOptionIndex = Math.floor(Math.random() * optionsWanted.length);
+            let randomOption = optionsWanted[randomOptionIndex - 1];
+            builtPasswordArr.push(randomOption);
+
+        }
+
+        //Write builtPassword to passwordBox
+            let builtPassword = builtPasswordArr.join('');
+                passwordBox.innerHTML =builtPassword); 
+        
+
+
+
 
     //Console.logs to see if things are working
     console.log(lengthWant);
@@ -61,15 +65,16 @@ generateBtn.addEventListener("click", function () {
     console.log(optionsWanted);
     console.log(builtPassword);
     console.log(builtPasswordArr);
-    console.log(password);
+    console.log(builtPassword)
+    
 
     
- 
+    } else {
+        alert("Please enter a number between 8 and 128");
+    }
 
 
 });
-
-
 
 
 //Possibly change passwordBox to builtPassword
